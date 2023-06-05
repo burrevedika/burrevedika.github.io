733f4caac2a6dab8569e2db1e65459a551437b85
VEDIKA <97499734+burrevedika@users.noreply.github.com>
2023-06-05 22:26:25 +0530

    added js

--- usercard2.js
+++ usercard2.js
@@ -1,5 +1,29 @@
 let mhdoldinfo={
     imgurl:"https://th.bing.com/th/id/OIP.N5gJ94vPwP29yVrJxqyK_gHaJ3?pid=ImgDet&rs=1",
     name:"Mahendra singh Dhoni",
-    Description:"Cricketer"
+    Description:"Ticket collector"
+}
+let mhdyounginfo{
+    imgurl:"https://media.gqindia.com/wp-content/uploads/2018/05/26-866x956.jpg",
+    name:"Dhoni",
+    description:"Cricketer"
+
+}
+let ismhdoldinf=true;
+let displayobject;
+let flipdata=function(){
+    if (ismhdoldinf==true){
+        displayobject=mhdyounginfo;
+        ismhdoldinf=false;
+    }
+    else{
+        displayobject=mhdoldinfo;
+        ismhdoldinf=true;
+    }
+    document.getElementById("dhoni-img").src=displayobject.imgurl;
+    document.getElementById("dhoniimg").innerHTML=displayobject.name;
+    document.getElementById("desc").innerHTML=displayobject.Description;
+    
+    
+
 }
