7434ba6debd791c4fd798106c2c44496bff288c2
VEDIKA <97499734+burrevedika@users.noreply.github.com>
2023-06-13 22:23:58 +0530

    added

--- usercard3.js
+++ usercard3.js
@@ -10,9 +10,10 @@ let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
     .then(data=>{
-        displayobject.name=data.results[0].name.first + " " + data.results[0].name.last
-        displayobject.imageurl=data.results[0].picture.large
-        displayobject.description=data.results[0].gender
+        //displayobject.name=data.results[0].name.first + " " + data.results[0].name.last
+        displayobject.name = "abcd"
+       // displayobject.imageurl=data.results[0].picture.large
+       // displayobject.description=data.results[0].gender
         document.getElementById("MSD-img").src=displayobject.imgurl;
         document.getElementById("MSD-name").innerHTML=displayobject.name;
         document.getElementById("MSD-desc").innerHTML=displayobject.description; 
