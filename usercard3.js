d6fb13d202f4e2aff39ae917a47df5ef306697ba
VEDIKA <97499734+burrevedika@users.noreply.github.com>
2023-06-13 22:27:48 +0530

    added

--- usercard3.js
+++ usercard3.js
@@ -5,7 +5,11 @@ let MSDhonioldinfo={
 } 
 
 
-let displayobject;
+let displayobject={
+    name:"",
+    imageurl:"",
+    description:""
+};
 let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
